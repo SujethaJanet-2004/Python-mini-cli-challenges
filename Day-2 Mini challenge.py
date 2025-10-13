f4896bbd91be66b54025ba32c4dc08470754117e
Sujetha Janet <ssujethajanet@gmail.com>
2025-10-13 19:53:26 +0530

    Add files via upload

---
+++ Day-2 Mini challenge.py
@@ -0,0 +1,17 @@
+# Mini Challenge: Operators
+
+num_1 = float(input("Enter Number 1: "))
+num_2 = float(input("Enter Number 2: "))
+add = num_1 + num_2
+sub = num_1 - num_2
+multiply = num_1 * num_2
+division = num_1 / num_2
+print("Sum:", add)
+print("Difference:", sub)
+print("Product:", multiply)
+print("Quotient:", division)
+if num_1 > num_2:
+    print("The greatest of the two numbers is:", num_1)
+else:
+    print("The greatest of the two numbers is:", num_2)
+
