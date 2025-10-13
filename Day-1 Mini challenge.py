9e88284f8e0c44c18da486507077f207288b7a71
Sujetha Janet <ssujethajanet@gmail.com>
2025-10-13 19:54:38 +0530

    Add files via upload

---
+++ Day-1 Mini challenge.py
@@ -0,0 +1,5 @@
+# Mini Challenge: Variable and Datatypes , Input and output
+
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+print(f"Hello {name},you are {age} years old!")
