5fa58edc60b96b99905fd284d59e8db67a94539b
Sujetha Janet <ssujethajanet@gmail.com>
2025-10-22 17:27:33 +0530

    Add files via upload

---
+++ Day-8 Mini challenge.py
@@ -0,0 +1,11 @@
+# Recursion 
+
+def climb_stairs(n):
+    if n ==1:
+        return 1
+    if n==2:
+        return 2
+    return climb_stairs(n-1) + climb_stairs(n-2)
+n = int(input("Enter number of steps: "))
+result = climb_stairs(n)
+print(result)
